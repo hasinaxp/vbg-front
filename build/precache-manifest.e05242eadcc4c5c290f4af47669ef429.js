07962edf9f9670120c0dd1a098790d6d1fadaa06
visakh.cs <visakh.cs@iinerds.com>
2019-03-23 20:49:00 +0530

    issue

---
+++ build/precache-manifest.e05242eadcc4c5c290f4af47669ef429.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "47b9886e6f8ccc3b588a",
+    "url": "/static/css/main.defe6061.chunk.css"
+  },
+  {
+    "revision": "c7cab73356240a3eaf1154eccacd8a6c",
+    "url": "/static/media/sword.c7cab733.svg"
+  },
+  {
+    "revision": "ba44325425ffb33a8b2b",
+    "url": "/static/js/1.ba443254.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fd55f4418a9716bde69b232dac822934",
+    "url": "/static/media/default.fd55f441.jpg"
+  },
+  {
+    "revision": "1f26fa90413a18dd26aad7b349c90189",
+    "url": "/static/media/vbg-back.1f26fa90.png"
+  },
+  {
+    "revision": "47b9886e6f8ccc3b588a",
+    "url": "/static/js/main.47b9886e.chunk.js"
+  },
+  {
+    "revision": "9563f3f6493ceee9023bfbadaae80af3",
+    "url": "/static/media/shot_2.9563f3f6.jpg"
+  },
+  {
+    "revision": "d5c477713c316d627fff41c497b77fdf",
+    "url": "/static/media/shot_1.d5c47771.jpg"
+  },
+  {
+    "revision": "c285721f99fa7cb43411274626cba7cf",
+    "url": "/static/media/8ball.c285721f.jpg"
+  },
+  {
+    "revision": "708c03cc7cadd2a3ef48d6aea10f336e",
+    "url": "/static/media/chess.708c03cc.jpg"
+  },
+  {
+    "revision": "75071ebe0c53b6f871ec035b5f872be3",
+    "url": "/static/media/vbg1.75071ebe.png"
+  },
+  {
+    "revision": "ba44325425ffb33a8b2b",
+    "url": "/static/css/1.331553c8.chunk.css"
+  },
+  {
+    "revision": "0e03015782edf37fba06b2508414624d",
+    "url": "/index.html"
+  }
+];
